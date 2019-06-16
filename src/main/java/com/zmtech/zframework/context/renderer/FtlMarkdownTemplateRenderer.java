9f5d715776f41c6e7b3e76149530bf81d09713fb
李博 <zimsky@163.com>
2019-06-16 19:59:21 +0800

    修改项目名称 提升到整体框架层面啦，牛逼啊

--- src/main/java/com/zmtech/zframework/context/renderer/FtlMarkdownTemplateRenderer.java
+++ src/main/java/com/zmtech/zframework/context/renderer/FtlMarkdownTemplateRenderer.java
@@ -1,12 +1,12 @@
 /*
- * This software is in the public domain under CC0 1.0 Universal plus a 
+ * This software is in the public domain under CC0 1.0 Universal plus a
  * Grant of Patent License.
- * 
+ *
  * To the extent possible under law, the author(s) have dedicated all
  * copyright and related and neighboring rights to this software to the
  * public domain worldwide. This software is distributed without any
  * warranty.
- * 
+ *
  * You should have received a copy of the CC0 Public Domain Dedication
  * along with this software (see the LICENSE.md file). If not, see
  * <http://creativecommons.org/publicdomain/zero/1.0/>.
@@ -26,6 +26,9 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 import javax.cache.Cache
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.Writer;
 
 @CompileStatic
 class FtlMarkdownTemplateRenderer implements TemplateRenderer {
--- src/main/java/com/zmtech/zframework/context/renderer/GStringTemplateRenderer.java
+++ src/main/java/com/zmtech/zframework/context/renderer/GStringTemplateRenderer.java
@@ -1,18 +1,19 @@
 /*
- * This software is in the public domain under CC0 1.0 Universal plus a 
+ * This software is in the public domain under CC0 1.0 Universal plus a
  * Grant of Patent License.
- * 
+ *
  * To the extent possible under law, the author(s) have dedicated all
  * copyright and related and neighboring rights to this software to the
  * public domain worldwide. This software is distributed without any
  * warranty.
- * 
+ *
  * You should have received a copy of the CC0 Public Domain Dedication
  * along with this software (see the LICENSE.md file). If not, see
  * <http://creativecommons.org/publicdomain/zero/1.0/>.
  */
 package com.zmtech.zframework.context.renderer
 
+import groovy.lang.Writable;
 import groovy.text.GStringTemplateEngine
 import groovy.text.Template
 import groovy.transform.CompileStatic
@@ -26,6 +27,9 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 import javax.cache.Cache
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.Writer;
 
 @CompileStatic
 class GStringTemplateRenderer implements TemplateRenderer {
