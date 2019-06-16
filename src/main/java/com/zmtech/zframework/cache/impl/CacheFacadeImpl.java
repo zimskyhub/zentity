706d273116ba49aea60d1eb37cefb9a2c24e8045
bo <zimskybo@icloud.com>
2019-06-17 00:09:36 +0800

    添加部分依赖和引用，还有很多啊加个油

--- src/main/java/com/zmtech/zframework/cache/impl/CacheFacadeImpl.java
+++ src/main/java/com/zmtech/zframework/cache/impl/CacheFacadeImpl.java
@@ -21,7 +21,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.TimeUnit;
 
-
+@SuppressWarnings("unused")
 public class CacheFacadeImpl implements CacheFacade {
     protected final static Logger logger = LoggerFactory.getLogger(CacheFacadeImpl.class);
 
@@ -36,15 +36,15 @@ public class CacheFacadeImpl implements CacheFacade {
         this.ecfi = ecfi;
 
         MNode cacheListNode = ecfi.getConfXmlRoot().first("cache-list");
-        String localCacheFactoryName = cacheListNode.attribute("local-factory") != null ?cacheListNode.attribute("local-factory"): ZCacheToolFactory.TOOL_NAME;
-                localCacheManagerInternal = ecfi.getTool(localCacheFactoryName, CacheManager.class);
+        String localCacheFactoryName = cacheListNode.attribute("local-factory") != null ? cacheListNode.attribute("local-factory") : ZCacheToolFactory.TOOL_NAME;
+        localCacheManagerInternal = ecfi.getTool(localCacheFactoryName, CacheManager.class);
     }
 
     public CacheManager getDistCacheManager() {
         if (distCacheManagerInternal == null) {
             MNode cacheListNode = ecfi.getConfXmlRoot().first("cache-list");
-            String distCacheFactoryName = cacheListNode.attribute("distributed-factory") != null  ? cacheListNode.attribute("distributed-factory"): ZCacheToolFactory.TOOL_NAME;
-                    distCacheManagerInternal = ecfi.getTool(distCacheFactoryName, CacheManager.class);
+            String distCacheFactoryName = cacheListNode.attribute("distributed-factory") != null ? cacheListNode.attribute("distributed-factory") : ZCacheToolFactory.TOOL_NAME;
+            distCacheManagerInternal = ecfi.getTool(distCacheFactoryName, CacheManager.class);
         }
         return distCacheManagerInternal;
     }
@@ -52,17 +52,19 @@ public class CacheFacadeImpl implements CacheFacade {
     public void destroy() {
         if (localCacheManagerInternal != null) {
             for (String cacheName : localCacheManagerInternal.getCacheNames())
-            localCacheManagerInternal.destroyCache(cacheName);
+                localCacheManagerInternal.destroyCache(cacheName);
         }
         localCacheMap.clear();
         if (distCacheManagerInternal != null) {
             for (String cacheName : distCacheManagerInternal.getCacheNames())
-            distCacheManagerInternal.destroyCache(cacheName);
+                distCacheManagerInternal.destroyCache(cacheName);
         }
     }
 
     @Override
-    public void clearAllCaches() { for (Cache cache : localCacheMap.values()) cache.clear(); }
+    public void clearAllCaches() {
+        for (Cache cache : localCacheMap.values()) cache.clear();
+    }
 
     @Override
     public void clearCachesByPrefix(String prefix) {
@@ -76,7 +78,10 @@ public class CacheFacadeImpl implements CacheFacade {
     }
 
     @Override
-    public Cache getCache(String cacheName) { return getCacheInternal(cacheName, "local"); }
+    public Cache getCache(String cacheName) {
+        return getCacheInternal(cacheName, "local");
+    }
+
     @Override
     public <K, V> Cache<K, V> getCache(String cacheName, Class<K> keyType, Class<V> valueType) {
         return getCacheInternal(cacheName, "local");
@@ -86,6 +91,7 @@ public class CacheFacadeImpl implements CacheFacade {
     public ZCache getLocalCache(String cacheName) {
         return (ZCache) getCacheInternal(cacheName, "local").unwrap(ZCache.class);
     }
+
     @Override
     public Cache getDistributedCache(String cacheName) {
         return getCacheInternal(cacheName, "distributed");
@@ -107,9 +113,14 @@ public class CacheFacadeImpl implements CacheFacade {
     }
 
     @Override
-    public boolean cacheExists(String cacheName) { return localCacheMap.containsKey(cacheName); }
+    public boolean cacheExists(String cacheName) {
+        return localCacheMap.containsKey(cacheName);
+    }
+
     @Override
-    public Set<String> getCacheNames() { return localCacheMap.keySet(); }
+    public Set<String> getCacheNames() {
+        return localCacheMap.keySet();
+    }
 
     public List<Map<String, Object>> getAllCachesInfo(String orderByField, String filterRegexp) {
         boolean hasFilterRegexp = filterRegexp != null && filterRegexp.length() > 0;
@@ -137,39 +148,40 @@ public class CacheFacadeImpl implements CacheFacade {
             } else
             */
             if (co instanceof ZCache) {
-                ZCache mc = (ZCache)co.unwrap(ZCache.class);
+                ZCache mc = (ZCache) co.unwrap(ZCache.class);
                 ZStats stats = mc.getMStats();
-                Long expireIdle = mc.getAccessDuration() != null ?mc.getAccessDuration().getDurationAmount() : 0;
-                Long expireLive = mc.getCreationDuration() != null ?mc.getCreationDuration().getDurationAmount() : 0;
-                ci.add(new ConcurrentHashMap<String,Object>(){{
-                    put("name",co.getName());
-                    put("expireTimeIdle",expireIdle);
-                    put("expireTimeLive",expireLive);
-                    put("maxElements",mc.getMaxEntries());
-                    put("evictionStrategy","LRU");
-                    put("size",mc.size());
-                    put("getCount",stats.getCacheGets());
-                    put("putCount",stats.getCachePuts());
-                    put("hitCount",stats.getCacheHits());
-                    put("missCountTotal",stats.getCacheMisses());
-                    put("evictionCount",stats.getCacheEvictions());
-                    put("removeCount",stats.getCacheRemovals());
-                    put("expireCount",stats.getCacheExpires());
+                Long expireIdle = mc.getAccessDuration() != null ? mc.getAccessDuration().getDurationAmount() : 0;
+                Long expireLive = mc.getCreationDuration() != null ? mc.getCreationDuration().getDurationAmount() : 0;
+                ci.add(new ConcurrentHashMap<String, Object>() {{
+                    put("name", co.getName());
+                    put("expireTimeIdle", expireIdle);
+                    put("expireTimeLive", expireLive);
+                    put("maxElements", mc.getMaxEntries());
+                    put("evictionStrategy", "LRU");
+                    put("size", mc.size());
+                    put("getCount", stats.getCacheGets());
+                    put("putCount", stats.getCachePuts());
+                    put("hitCount", stats.getCacheHits());
+                    put("missCountTotal", stats.getCacheMisses());
+                    put("evictionCount", stats.getCacheEvictions());
+                    put("removeCount", stats.getCacheRemovals());
+                    put("expireCount", stats.getCacheExpires());
                 }});
             } else {
-                logger.warn("Cannot get detailed info for cache ${cn} which is of type ${co.class.name}");
+                logger.warn("无法获取名称为: [" + cn + "] 类型为: [" + co.getClass().getName() + "] 的详细信息");
             }
         }
-        if (orderByField != null && !orderByField.isEmpty()) CollectionUtil.orderMapList(ci, Arrays.asList(orderByField));
+        if (orderByField != null && !orderByField.isEmpty())
+            CollectionUtil.orderMapList(ci, Collections.singletonList(orderByField));
         return ci;
     }
 
     protected MNode getCacheNode(String cacheName) {
         MNode cacheListNode = ecfi.getConfXmlRoot().first("cache-list");
-        MNode cacheElement = cacheListNode.first( (MNode it) -> { return it.getName().equals("cache")  && it.attribute("name").equals(cacheName);});
+        MNode cacheElement = cacheListNode.first((MNode it) -> it.getName().equals("cache") && it.attribute("name").equals(cacheName));
         // nothing found? try starts with, ie allow the cache configuration to be a prefix
         if (cacheElement == null) cacheElement = cacheListNode
-                .first((MNode it) ->{ return it.getName().equals("cache") && cacheName.startsWith(it.attribute("name"));});
+                .first((MNode it) -> it.getName().equals("cache") && cacheName.startsWith(it.attribute("name")));
         return cacheElement;
     }
 
@@ -181,13 +193,13 @@ public class CacheFacadeImpl implements CacheFacade {
         Cache newCache;
         MNode cacheNode = getCacheNode(cacheName);
         if (cacheNode != null) {
-            String keyTypeName = cacheNode.attribute("key-type") != null ? cacheNode.attribute("key-type"): "String";
-            String valueTypeName = cacheNode.attribute("value-type") != null ?  cacheNode.attribute("value-type"): "Object";
+            String keyTypeName = cacheNode.attribute("key-type") != null ? cacheNode.attribute("key-type") : "String";
+            String valueTypeName = cacheNode.attribute("value-type") != null ? cacheNode.attribute("value-type") : "Object";
             Class keyType = ObjectUtil.getClass(keyTypeName);
             Class valueType = ObjectUtil.getClass(valueTypeName);
 
             Factory<ExpiryPolicy> expiryPolicyFactory;
-            if (cacheNode.attribute("expire-time-idle") != null && !cacheNode.attribute("expire-time-idle").equals("0") ) {
+            if (cacheNode.attribute("expire-time-idle") != null && !cacheNode.attribute("expire-time-idle").equals("0")) {
                 expiryPolicyFactory = AccessedExpiryPolicy.factoryOf(
                         new Duration(TimeUnit.SECONDS, Long.parseLong(cacheNode.attribute("expire-time-idle"))));
             } else if (cacheNode.attribute("expire-time-live") != null && !cacheNode.attribute("expire-time-live").equals("0")) {
@@ -197,14 +209,14 @@ public class CacheFacadeImpl implements CacheFacade {
                 expiryPolicyFactory = EternalExpiryPolicy.factoryOf();
             }
 
-            String cacheType = cacheNode.attribute("type")!=null ?cacheNode.attribute("type"): defaultCacheType;
+            String cacheType = cacheNode.attribute("type") != null ? cacheNode.attribute("type") : defaultCacheType;
             CacheManager cacheManager;
             if ("local".equals(cacheType)) {
                 cacheManager = localCacheManagerInternal;
             } else if ("distributed".equals(cacheType)) {
                 cacheManager = getDistCacheManager();
             } else {
-                throw new IllegalArgumentException("Cache type ${cacheType} not supported");
+                throw new IllegalArgumentException("不支持 [" + cacheType + "] 类型的缓存");
             }
 
             Configuration config;
@@ -216,12 +228,12 @@ public class CacheFacadeImpl implements CacheFacade {
                 mConf.setExpiryPolicyFactory(expiryPolicyFactory);
 
                 String maxElementsStr = cacheNode.attribute("max-elements");
-                if (maxElementsStr != null && !maxElementsStr.equals( "0")) {
+                if (maxElementsStr != null && !maxElementsStr.equals("0")) {
                     int maxElements = Integer.parseInt(maxElementsStr);
                     mConf.setMaxEntries(maxElements);
                 }
 
-                config = (Configuration) mConf;
+                config = mConf;
             /* TODO: somehow support external cache configuration like Hazelcast, through some sort of Moqui interface, maybe pass cacheNode to Cache factory?
                NOTE: this isn't all that important because we don't have a good use case for distributed caches, and they can be configured directly through hazelcast.xml or other Hazelcast conf
             } else if (cacheManager instanceof AbstractHazelcastCacheManager) {
@@ -246,7 +258,7 @@ public class CacheFacadeImpl implements CacheFacade {
                 config = (Configuration) cacheConfig
             */
             } else {
-                logger.info("Initializing cache "+cacheName+" which has a CacheManager of type "+cacheManager.getClass().getName()+" and extended configuration not supported, using simple MutableConfigutation");
+                logger.info("初始化缓存: [" + cacheName + "] 缓存管理器类型为: [" + cacheManager.getClass().getName() + "] 不支持扩展配置选项, 正在使用 Mutable Configuration");
                 MutableConfiguration mutConfig = new MutableConfiguration();
                 mutConfig.setTypes(keyType, valueType);
                 mutConfig.setStoreByValue("distributed".equals(cacheType)).setStatisticsEnabled(true);
@@ -266,10 +278,10 @@ public class CacheFacadeImpl implements CacheFacade {
                 cacheManager = getDistCacheManager();
                 storeByValue = true;
             } else {
-                throw new IllegalArgumentException("Default cache type ${defaultCacheType} not supported");
+                throw new IllegalArgumentException("不支持默认缓存类型" + defaultCacheType + ",请修改默认缓存类型: defaultCacheType");
             }
 
-            logger.info("Creating default ${defaultCacheType} cache ${cacheName}, storeByValue=${storeByValue}");
+            logger.info("正在创建默认类型为: [" + defaultCacheType + "] 缓存名称为: [" + cacheName + "], storeByValue:=${storeByValue}");
             MutableConfiguration mutConfig = new MutableConfiguration();
             mutConfig.setStoreByValue(storeByValue).setStatisticsEnabled(true);
             // any defaults we want here? better to use underlying defaults and conf file settings only
@@ -280,24 +292,25 @@ public class CacheFacadeImpl implements CacheFacade {
         return newCache;
     }
 
-    public List<Map<String,Object>> makeElementInfoList(String cacheName, String orderByField) {
+    public List<Map<String, Object>> makeElementInfoList(String cacheName, String orderByField) {
         Cache cache = getCache(cacheName);
         if (cache instanceof ZCache) {
-            ZCache mCache = (ZCache)cache.unwrap(ZCache.class);
-            List<Map<String,Object>> elementInfoList = new ArrayList<>();
-            for (Cache.Entry ce : mCache.getEntryList()) {
-                ZEntry entry = (ZEntry)ce.unwrap(ZEntry.class);
-                Map<String, Object> im = new HashMap<String, Object>(){{
-                    put("key",entry.getKey());
-                    put("value",entry.getValue());
-                    put("hitCount",entry.getAccessCount());
-                    put("creationTime",new Timestamp(entry.getCreatedTime()));
+            ZCache mCache = (ZCache) cache.unwrap(ZCache.class);
+            List<Map<String, Object>> elementInfoList = new ArrayList<>();
+            for (Object ce : mCache.getEntryList()) {
+                ZEntry entry = (ZEntry) ((Cache.Entry) ce).unwrap(ZEntry.class);
+                Map<String, Object> im = new HashMap<String, Object>() {{
+                    put("key", entry.getKey());
+                    put("value", entry.getValue());
+                    put("hitCount", entry.getAccessCount());
+                    put("creationTime", new Timestamp(entry.getCreatedTime()));
                 }};
-                if (entry.getLastUpdatedTime() > 0)im.put("lastUpdateTime",new Timestamp(entry.getLastUpdatedTime()));
-                if (entry.getLastAccessTime() > 0)im.put("lastAccessTime",new Timestamp(entry.getLastAccessTime()));
+                if (entry.getLastUpdatedTime() > 0) im.put("lastUpdateTime", new Timestamp(entry.getLastUpdatedTime()));
+                if (entry.getLastAccessTime() > 0) im.put("lastAccessTime", new Timestamp(entry.getLastAccessTime()));
                 elementInfoList.add(im);
             }
-            if (orderByField != null && !orderByField.isEmpty() ) CollectionUtil.orderMapList(elementInfoList, Arrays.asList(orderByField));
+            if (orderByField != null && !orderByField.isEmpty())
+                CollectionUtil.orderMapList(elementInfoList, Arrays.asList(orderByField));
             return elementInfoList;
         } else {
             return new ArrayList<>();
